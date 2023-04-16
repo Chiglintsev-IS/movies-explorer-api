77bb9677f7d45abfc1ce41b0c4a760a50cbca758
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2023-04-16 21:28:53 +0200

    new: extend user schema by methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../configs/config');
 
 const { Schema } = mongoose;
 
@@ -8,20 +11,18 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true,
+    lowercase: true,
     validate: {
       validator: (v) => validator.isEmail(v),
       message: 'Неправильный формат почты',
     },
   },
-  // хеш пароля
   password: {
     type: String,
-    validate: {
-      validator: (v) => validator.isStrongPassword(v),
-      message: 'Неправильный формат пароля',
-    },
     required: true,
     select: false,
+    minlength: 1,
   },
   name: {
     type: String,
@@ -30,5 +31,38 @@ const userSchema = new Schema({
     maxlength: 30,
   },
 });
+
+// Функция хеширования пароля перед сохранением пользователя
+userSchema.pre('save', async function hashPassrowrd(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Функция поиска пользователя по email и проверки пароля
+userSchema.statics.findUserByCredentials = async function findUserByCredentials(email, password) {
+  const user = await this.findOne({ email }).select('+password');
+  if (!user) {
+    throw new Error('Invalid email or password');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Invalid email or password');
+  }
+  return user;
+};
+
+// Функция генерации токена аутентификации для пользователя
+userSchema.methods.generateAuthToken = async function generateAuthToken() {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET);
+  return token;
+};
 
 module.exports = mongoose.model('user', userSchema);
