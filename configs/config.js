3f13e739c8b93377ec054c612d087c79fa9d4eeb
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2023-04-17 20:34:34 +0200

    fix: change DB url in config

--- configs/config.js
+++ configs/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/bitfilmsdb',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
   JWT_SECRET: process.env.JWT_SECRET || 'dev-secret',
 };
