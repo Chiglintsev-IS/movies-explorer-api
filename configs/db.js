795fa5a0252ad70bbcb297df2cfabf1e2ded20cd
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2023-04-17 20:03:42 +0200

    fix: add log for db connect

--- configs/db.js
+++ configs/db.js
@@ -5,5 +5,13 @@ module.exports = () => {
   mongoose.connect(
     MONGODB_URL,
     { useUnifiedTopology: true },
-  );
+  )
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('Database connected successfully');
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Error connecting to the database:', error.message);
+    });
 };
