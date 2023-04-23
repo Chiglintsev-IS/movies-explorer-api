543d5f13a89ba75296f7e29f07eff8cb8b67fe98
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2023-04-23 11:38:02 +0200

    fix: allow console.error in eslint rules

--- configs/db.js
+++ configs/db.js
@@ -6,12 +6,7 @@ module.exports = () => {
     MONGODB_URL,
     { useUnifiedTopology: true },
   )
-    .then(() => {
-      // eslint-disable-next-line no-console
-      console.log('Database connected successfully');
-    })
     .catch((error) => {
-      // eslint-disable-next-line no-console
       console.error('Error connecting to the database:', error.message);
     });
 };
